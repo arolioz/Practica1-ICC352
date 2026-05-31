6ff389a0540191f1b261f576c1edb6bfe9f28c11
AARON JOSÉ ESCAÑO GARCÍA <arolio301911@gmail.com>
2026-05-31 13:47:46 -0400

    Agregando metodo para obtener los headers de una url

--- src/main/java/edu/pucmm/eict/practica1/main.java
+++ src/main/java/edu/pucmm/eict/practica1/main.java
@@ -1,9 +1,18 @@
 package edu.pucmm.eict.practica1;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
 
         Scanner scanner = new Scanner(System.in);
 
@@ -14,5 +23,15 @@ public class main {
         System.out.println("La url es " + url);
 
         scanner.close();
+
+        HttpRequest request = HttpRequest.newBuilder().uri(new URI("https://postman-echo.com/get")).GET().build();
+
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        HttpHeaders responseHeaders = response.headers();
+
+        System.out.println(responseHeaders);
+
+        //HttpRequest.newBuilder(request, (name, value) -> !name.equalsIgnoreCase("Foo-Bar"));
+
     }
 }
