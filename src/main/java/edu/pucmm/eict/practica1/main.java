534e072f99259203dcd9695d84025aca1249f320
AARON JOSÉ ESCAÑO GARCÍA <arolio301911@gmail.com>
2026-05-31 13:32:33 -0400

    Configurando graddle para que utilize la entrada estandar

--- src/main/java/edu/pucmm/eict/practica1/main.java
+++ src/main/java/edu/pucmm/eict/practica1/main.java
@@ -1,5 +1,18 @@
 package edu.pucmm.eict.practica1;
 
-public class main {
+import java.util.Scanner;
 
+public class main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese la url: ");
+
+        String url = scanner.nextLine();
+
+        System.out.println("La url es " + url);
+
+        scanner.close();
+    }
 }
